9936a83f681ffae4381d951acf9ff75772040cbe
Armand <99232659+imsoujirou@users.noreply.github.com>
2024-03-21 00:05:32 +0800

    Initial commit

---
+++ basics1.js
@@ -0,0 +1,11 @@
+console.log('test');
+
+//To display the type of variable a which is a NUMBER
+let a=1
+console.log(typeof(a))
+
+//To display the type of variable b which stills a NUMBER even if the value is float/double
+let b=3.45
+console.log(typeof(b))
+
+let c="Dingdong Pogi"
