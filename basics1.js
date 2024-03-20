06ac80c08545cb508c768458323e881b3094efa7
Armand <99232659+imsoujirou@users.noreply.github.com>
2024-03-21 00:39:38 +0800

    First Day Again on learning javascript and cypress

--- basics1.js
+++ basics1.js
@@ -8,7 +8,24 @@ console.log(typeof(a))
 let b=3.45
 console.log(typeof(b))
 
-
 //To display the type of variable c which are string
 let c="Dingdong Pogi"
 console.log(typeof(c))
+
+//To display  the type of variable required which are boolean
+let required=true
+console.log(typeof(required))
+
+
+// Reusing variable is not allowed
+// let d=a+b
+// console.log(d)
+
+
+//This scenario can be used in re-assigning but if you use let, there will be an error on the terminal
+//Sample issue error: SyntaxError: Identifier 'c' has already been declared
+c=a+b
+console.log(c)
+
+//boolean symbol for "!" is not
+console.log(!required)
